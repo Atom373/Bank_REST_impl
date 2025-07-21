90c1b598ee32e2172d8b89693febdcc9356859c7
Atom <pvana7065@gmail.com>
2025-07-21 22:28:45 +0300

    Added security config

--- src/main/java/com/example/bankcards/repository/UserRepository.java
+++ src/main/java/com/example/bankcards/repository/UserRepository.java
@@ -1,12 +1,14 @@
 package com.example.bankcards.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.bankcards.entity.User;
 
 public interface UserRepository extends JpaRepository<User, Long>{
 
-	User findByEmail(String email);
+	Optional<User> findByEmail(String email);
 	
 	Boolean existsByEmail(String email);
 }
---
+++ src/main/java/com/example/bankcards/security/config/SecurityConfig.java
@@ -0,0 +1,70 @@
+package com.example.bankcards.security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.example.bankcards.repository.UserRepository;
+import com.example.bankcards.security.filter.JwtRequestFilter;
+
+import lombok.AllArgsConstructor;
+
+
+@Configuration
+@EnableWebSecurity
+@AllArgsConstructor
+public class SecurityConfig {
+
+	private JwtRequestFilter jwtRequestFilter;
+		
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	@Bean
+	public AuthenticationManager configureAuthenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+		return authConfig.getAuthenticationManager();
+	}
+	
+	@Bean
+	public UserDetailsService userDetailsService(UserRepository repo) {
+		return email -> {
+			return repo.findByEmail(email).orElseThrow(
+				() -> new UsernameNotFoundException("User with email " + email + " not found")
+			);
+		};
+	}
+	
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		return http
+				.csrf( csrf -> csrf.disable())
+				.cors(Customizer.withDefaults())
+				.authorizeHttpRequests( requests -> requests
+						.requestMatchers("/api/auth").permitAll()
+						.anyRequest().authenticated()
+				)
+				.sessionManagement( sessionManagement -> sessionManagement
+						.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+				)
+				.exceptionHandling( exceptionHandling -> exceptionHandling
+						.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+				) 
+				.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class)
+				.build();
+	}
+}
